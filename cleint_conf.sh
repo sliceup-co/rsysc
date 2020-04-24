a5fc825ca7a9fb57c0cc49f5af0fd56c7caee464
sliceup-co <brandon@sliceup.co>
2020-04-24 21:07:34 +0000

    First commit

---
+++ cleint_conf.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+clear
+echo "Enter the IP address of the remote rsyslog server"
+read rm_ip
+
+cat rsyslog.conf | sed "s/REMOTE_IP/$rm_ip/" | sudo tee /etc/rsyslog.conf
+
+sudo systemctl enable rsyslog
+sudo systemctl start rsyslog
+echo "verifying rsyslog is enabled"
+sleep 2
+sudo systemctl restart rsyslog
+
+# Begin sending files
+clear
+echo "Loading Log Files"
+sleep 2
+
+clear
+echo "Sending Andriod_2k" ; sleep 2
+
+cat logs/Andriod_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending BGL_2k" ; sleep 2
+
+cat logs/BGL_2k.log| tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending HPC_2k" ; sleep 2
+
+cat logs/HPC_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending HealthApp_2k" ; sleep 2
+
+cat logs/HealthApp_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Mac_2k" ; sleep 2
+
+cat logs/Mac_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending OpenStack_2k" ; sleep 2
+
+
+cat logs/OpenStack_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Spark_2k" ; sleep 2
+
+cat logs/Spark_2k.log  | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Windows_2k" ; sleep 2
+
+
+cat logs/Windows_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Apache_2k" ; sleep 2
+
+cat logs/Apache_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending HDFS_2k" ; sleep 2
+
+cat logs/HDFS_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Hadoop_2k" ; sleep 2
+
+cat logs/Hadoop_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Linux_2k" ; sleep 2
+
+cat logs/Linux_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending OpenSSH_2k" ; sleep 2
+
+cat logs/OpenSSH_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Proxifier_2k" ; sleep 2
+
+cat logs/Proxifier_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Thunderbird_2k" ; sleep 2
+
+cat logs/Thunderbird_2k.log | tee /tmp/test.log
+sleep 3
+
+clear
+echo "Sending Zookeeper_2k" ; sleep 2
+
+cat logs/Zookeeper_2k.log | tee /tmp/test.log
+sleep 3
